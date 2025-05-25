8706584c5595a5a0c5aeaf8afc4c09578e8adcc2
Gilang Risky M <gilangriskymahardhika@gmail.com>
2025-05-25 13:41:27 +0700

    tambah forced https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -28,5 +28,6 @@ class AppServiceProvider extends ServiceProvider
         Paginator::useBootstrapFive();
         Gate::policy(Blog::class, BlogPolicy::class);
         Gate::policy(User::class, UserPolicy::class);
+        // \URL::forceScheme('https');
     }
 }
